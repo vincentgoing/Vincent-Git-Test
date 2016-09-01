9f1581e710e773c72d00a8886700b4462f46c0c8
vincentgoing <vincent@gogoeshop.com>
2016-09-01 10:44:43 +0800

    Vincent Commit 001

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+if( !isset($_SERVER['PHP_AUTH_USER']) )
+{
+    $server_http_auth = isset($_SERVER["HTTP_AUTHORIZATION"])?$_SERVER["HTTP_AUTHORIZATION"]:$_SERVER["REDIRECT_HTTP_AUTHORIZATION"];
+    
+    list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($server_http_auth, 6)));
+    if( strlen($_SERVER['PHP_AUTH_USER']) == 0 || strlen($_SERVER['PHP_AUTH_PW']) == 0 )
+    {
+        unset($_SERVER['PHP_AUTH_USER']);
+        unset($_SERVER['PHP_AUTH_PW']);
+    }
+}
+
+include("xcache.php");
